8280df6c392831628e9b9fb6351ab8e32585c650
Ryan Vu <ryanvu1131@gmail.com>
2025-08-06 00:30:12 -0700

    popup events

---
+++ extension/popup.js
@@ -0,0 +1,35 @@
+document.getElementById("mainTab").addEventListener("click", () => {
+    document.getElementById("mainPage").style.display = "block";
+    document.getElementById("settingsPage").style.display = "none";
+    document.getElementById("mainTab").classList.add("active");
+    document.getElementById("settingsTab").classList.remove("active");
+});
+  
+  document.getElementById("settingsTab").addEventListener("click", () => {
+    document.getElementById("mainPage").style.display = "none";
+    document.getElementById("settingsPage").style.display = "block";
+    document.getElementById("mainTab").classList.remove("active");
+    document.getElementById("settingsTab").classList.add("active");
+});
+
+const features = ["guideBar", "dots", "timer", "bionic", "blink", "magnification", "lazy", "focus", "iso", "syllables", "cursor"];
+
+features.forEach((features) => {
+    const checkbox = document.getElementById(features);
+    checkbox.addEventListener('change', async (e) =>{
+        const [tab] = await chrome.tabs.query({ active: true, currentWindow: true});
+        chrome.tabs.sendMessage(tab.id, {
+            action: `toggle_${feature}`,
+            value: e.target.checked
+          });
+    });
+});
+
+document.getElementById('relaxButton').addEventListener('click', async () => {
+    const duration = parseInt(document.getElementById('relaxDuration'). value, 10) || 20;
+    const [tab] = await chrome.tabs.query({ active: true, currentWindow: true});
+    chrome.tabs.sendMessage(tab.id, {
+        action: 'trigger_relax_mode',
+        duration
+    });
+});

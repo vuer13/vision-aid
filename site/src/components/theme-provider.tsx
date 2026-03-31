c678dbe071aac7971f5a4ef87545befed457e763
Ryan Vu <ryanvu1131@gmail.com>
2026-03-31 01:03:05 -0700

    deleted site

--- site/src/components/theme-provider.tsx
+++
@@ -1,6 +0,0 @@
-"use client";
-import { ThemeProvider as NextThemes } from "next-themes";
-
-export function ThemeProvider({ children }: { children: React.ReactNode }) {
-    return <NextThemes attribute="class" defaultTheme="system" enableSystem>{children}</NextThemes>;
-}
